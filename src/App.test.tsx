b4a755f7f14aef948eb1decc7284c9e235748071
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-28 13:14:36 +0200

    Setup CRA Project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/* 
+Features:
+• Es wird angezeigt, wenn es noch gar keine To-dos gibt.
+• Es können neue To-dos angelegt werden.
+• ein To-do besteht mindestens aus einem freiwählbaren Titel 
+• Es werden alle OFFENen To-dos angezeigt
+• Ein To-do kann als "ERLEDIGT" markiert werden.
+• Wenn ein To-do als erledigt markiert wird, dann wird es nicht mehr angezeigt.
+• Die offenen To-dos werden auch bei einem Neuladen des Browsers wieder angezeigt. 
+*/
+
+test('"Implement me!"', () => {
+  throw new Error("Implement me!")
 });
