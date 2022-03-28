d47c30ab25b2b38687a65c6191b11b18b814d68b
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-28 14:08:34 +0200

    Add Tests for features

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
 /* 
 Features:
@@ -13,6 +14,170 @@ Features:
 • Die offenen To-dos werden auch bei einem Neuladen des Browsers wieder angezeigt. 
 */
 
-test('"Implement me!"', () => {
-  throw new Error("Implement me!")
+describe("App should,", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test("display 'keine To-Dos' when there are no To-Dos.", async () => {
+    render(<App />);
+    expect(await screen.findByText(/keine To-Dos/i)).toBeInTheDocument();
+  });
+
+  test("display an text input and create button to provide the ability to create new To-Dos.", async () => {
+    render(<App />);
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+    expect(textInput).toBeInTheDocument();
+    expect(createButton).toBeInTheDocument();
+  });
+
+  test("display the To-Do Title once it is created.", async () => {
+    render(<App />);
+    const titel = "Some random Text to test this thing.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+    await userEvent.type(textInput, titel);
+    await userEvent.click(createButton);
+
+    const todo = await screen.findByText(titel);
+    expect(todo).toBeInTheDocument();
+  });
+
+  test("provide an option to mark the To-Do as Erledigt.", async () => {
+    render(<App />);
+    const titel = "Some random Text to test this thing.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+    await userEvent.type(textInput, titel);
+    await userEvent.click(createButton);
+
+    const doneButton = await screen.findByRole("button", { name: /erledigt/i });
+    await userEvent.click(doneButton);
+    const todo = await screen.findByText(titel);
+    expect(todo).toHaveStyle("text-decoration: line-through;");
+  });
+
+  test("display only the open To-Dos.", async () => {
+    render(<App />);
+    const openTodoText = "Open.";
+    const closedTodoText = "Closed.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+
+    await userEvent.type(textInput, closedTodoText);
+    await userEvent.click(createButton);
+
+    const doneButton = await screen.findByRole("button", { name: /erledigt/i });
+    await userEvent.click(doneButton);
+
+    await userEvent.type(textInput, openTodoText);
+    await userEvent.click(createButton);
+
+    const open = await screen.findByText(openTodoText);
+    const closed = await screen.findByText(closedTodoText);
+
+    expect(open).toBeVisible();
+    expect(closed).not.toBeVisible();
+  });
+
+  test("hide the closed To-Dos behind an accordion.", async () => {
+    render(<App />);
+    const openTodoText = "Open.";
+    const closedTodoText = "Closed.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+
+    await userEvent.type(textInput, closedTodoText);
+    await userEvent.click(createButton);
+
+    const doneButton = await screen.findByRole("button", { name: /erledigt/i });
+    await userEvent.click(doneButton);
+
+    await userEvent.type(textInput, openTodoText);
+    await userEvent.click(createButton);
+
+    const accordionButton = await screen.findByRole("button", {
+      name: /zeige erledigte/i,
+    });
+
+    expect(accordionButton).toBeInTheDocument();
+  });
+
+  test("show the closed To-Dos when the accordion is clicked.", async () => {
+    render(<App />);
+    const openTodoText = "Open.";
+    const closedTodoText = "Closed.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+
+    await userEvent.type(textInput, closedTodoText);
+    await userEvent.click(createButton);
+
+    const doneButton = await screen.findByRole("button", { name: /erledigt/i });
+    await userEvent.click(doneButton);
+
+    await userEvent.type(textInput, openTodoText);
+    await userEvent.click(createButton);
+
+    const closed = await screen.findByText(closedTodoText);
+    const accordionButton = await screen.findByRole("button", {
+      name: /zeige erledigte/i,
+    });
+
+    expect(closed).not.toBeVisible();
+    await userEvent.click(accordionButton)
+    expect(closed).toBeVisible();
+  });
+
+  test("save the created To-Dos to local Storage.", async () => {
+    render(<App />);
+    const openTodoText = "Open.";
+    const closedTodoText = "Closed.";
+    const textInput = await screen.findByRole("textbox", { name: /titel/i });
+    const createButton = await screen.findByRole("button", {
+      name: /erstellen/i,
+    });
+
+    await userEvent.type(textInput, closedTodoText);
+    await userEvent.click(createButton);
+
+    const doneButton = await screen.findByRole("button", { name: /erledigt/i });
+    await userEvent.click(doneButton);
+
+    await userEvent.type(textInput, openTodoText);
+    await userEvent.click(createButton);
+
+    const todos = localStorage.getItem("todos");
+    expect(todos).toBeDefined()
+    if(!todos) throw Error("todo is not defined.")
+
+    expect(JSON.parse(todos)).toMatchObject([{titel: openTodoText, open: true},{titel: closedTodoText, open: false}])
+  });
+
+  test("load the To-Dos from local Storage.", async () => {
+    const openTodoText = "Open.";
+    const closedTodoText = "Closed.";
+
+    localStorage.setItem("todos", JSON.stringify([{titel: openTodoText, open: true},{titel: closedTodoText, open: false}]))
+
+    render(<App />);
+    const open = await screen.findByText(openTodoText);
+    const closed = await screen.findByText(closedTodoText);
+
+    expect(open).toBeVisible();
+    expect(closed).not.toBeVisible();
+  });
 });
