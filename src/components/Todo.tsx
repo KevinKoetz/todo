78f86156f639e0773d5b6768e378b07d390d69cf
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-28 17:15:31 +0200

    Create Todo Component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,30 +1,51 @@
-import { ListItem, Typography } from "@mui/material";
+import { ListItem, Typography, IconButton, Box } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import UndoIcon from "@mui/icons-material/Undo";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface TodoProps {
   titel: string;
   open: boolean;
-  onDone: () => void;
-  onUndone: () => void;
+  onClose?: () => void;
+  onReopen?: () => void;
+  onDelete?: () => void;
 }
 
-function Todo({ titel, open, onDone, onUndone }: TodoProps) {
+function Todo({ titel, open, onClose, onReopen, onDelete }: TodoProps) {
   return (
     <ListItem
       sx={{
-        textDecoration: open ? undefined : "line-through",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
       }}
     >
-      <Typography>{titel}</Typography>{" "}
+      <Typography sx={{textDecoration: open ? undefined : "line-through"}}>{titel}</Typography>
+      <Box>
       {open ? (
-        <DoneIcon sx={{ color: "success.main" }} onClick={onDone} />
+        <IconButton
+          color="success"
+          aria-label="Als erledigt markieren."
+          onClick={onClose}
+        >
+          <DoneIcon />
+        </IconButton>
       ) : (
-        <UndoIcon sx={{ color: "warning.main" }} onClick={onUndone} />
+        <IconButton
+          color="warning"
+          aria-label="Als offen markieren."
+          onClick={onReopen}
+        >
+          <UndoIcon />
+        </IconButton>
       )}
+      <IconButton color="error"
+          aria-label="Löschen."
+          onClick={onDelete}>
+        <DeleteIcon />
+      </IconButton>
+      </Box>
+      
     </ListItem>
   );
 }
